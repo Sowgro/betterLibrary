4d041a58a53f2bad9ccc90645b179bc787f7173d
Sowgro <69283684+Sowgro@users.noreply.github.com>
2023-09-15 20:53:52 -0400

    Bug fixes

--- app/loadcss.js
+++ app/loadcss.js
@@ -2,4 +2,11 @@
     document.getElementsByTagName('head')[0].insertAdjacentHTML(
         'beforeend',
         '<link rel="stylesheet" href="spicetify-routes-betterlibrary.css" />');
+    // disable after window is closed to prevent an expanded sidebar on opening
+    window.addEventListener(
+        'beforeunload',
+        (event) => {
+            disableBetterLib();
+        }
+        )
 })();
